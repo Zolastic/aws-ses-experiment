0234f197271096a978a63837d64f653938a707e7
Zolastic <honhattienlul@gmail.com>
2024-04-02 22:16:29 +0800

    refactor: Update sender email to use environment variable

--- src/components/SendEmailButton.tsx
+++ src/components/SendEmailButton.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import { Button } from "./ui/button";
 import { processEmail } from "@/lib/email-utils";
 import { toast } from "sonner";
+import { env } from "@/env";
 
 type Props = {
   recipients: string[];
 };
 
 const SendEmailButton = ({ recipients }: Props) => {
-  const sender = "honhattienlul@gmail.com";
+  const sender = env.NEXT_PUBLIC_SENDER_EMAIL;
   const subject = "Hello from AWS SES";
   const content = "<h1>Hello from AWS SES APP</h1>";
 
